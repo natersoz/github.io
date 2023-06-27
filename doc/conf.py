6bb579542265f34d893e247d2fe33fa94a0f926d
Nat Ersoz <nersoz@impinj.com>
2023-06-27 10:49:47 -0700

    Use html theme: alabaster

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,9 @@ sys.path.insert(0, os.path.abspath('../source'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,9 +48,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'WordCount'
-copyright = '2018, Harsha'
-author = 'Harsha'
+project = 'backbias blog'
+copyright = '2022, Nat Ersoz'
+author = 'Nat Ersoz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -83,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -164,6 +166,3 @@ texinfo_documents = [
      author, 'WordCount', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
